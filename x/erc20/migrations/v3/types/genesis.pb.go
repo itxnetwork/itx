cec06928a0fb9d49151795cea832f4d8c2c046b9
Tomi Varga <tomi.varga@interticket.hu>
2023-11-16 23:21:32 +0100

    typo fix

--- x/erc20/migrations/v3/types/genesis.pb.go
+++ x/erc20/migrations/v3/types/genesis.pb.go
@@ -10,7 +10,7 @@ import (
 	math_bits "math/bits"
 
 	_ "github.com/cosmos/gogoproto/gogoproto"
-	"github.com/itxtitxitx/v12/x/erc20/types"
+	"github.com/itxnetwork/itx/v12/x/erc20/types"
 	proto "github.com/gogo/protobuf/proto"
 )
 
